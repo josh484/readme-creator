caff97044594dbb44131c5ca6e486df734183ae2
unknown <josh.m.mendoza1@gmail.com>
2024-01-12 19:50:24 +0000

    finished markdown layout

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,31 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return  `
+    ${data.lisence}
     # ${data.title}
     ## Description
     ${data.description}
     ## Table of Contents
     [Installation](#installation)
+    [Usage](#usage)
+    [Lisence](#lisence)
+    [Contribution](#contribution)
+    [Tests](#tests)
+    [Questions](#questions)
     ## Installation
     ${data.installation}
     ## Usage
     ${data.usage}
+    ## Lisence
+    ${data.lisenceD}
+    ## Contribution
+    ${data.contribution}
+    ## Tests
+    ${data.tests}
+    ## Questions
+    Github username: ${data.gitname}
+    github.com/${data.gitname}
+    Reach out to me for additional instructions at: ${data.email}
 `;
 }
 
