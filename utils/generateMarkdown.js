a488dd0294bb39d996c512bd5cc9b60dabf8e338
unknown <josh.m.mendoza1@gmail.com>
2024-01-12 20:26:21 +0000

    completed function for lisences

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -33,6 +33,15 @@ function lisence(lisence){
   if (lisence == 'MIT'){
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   }
+  else if (lisence == 'Mozilla'){
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  } 
+  else if (lisence == 'GNU GPL V3'){
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  else if (lisence == 'BSD 3-Clause Lisence') {
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+  }
 }
 
 module.exports = generateMarkdown;
