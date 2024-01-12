92d7de51cbc7d8dcfebe9cd4b1d7dd6689a2915b
unknown <josh.m.mendoza1@gmail.com>
2024-01-12 18:37:32 +0000

    installed inquirer, created fs append file for readme

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    title
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.appendFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
 }
 
 // function to initialize program
